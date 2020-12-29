c971f5c5fb3e769466fedde77cce57d89e927499
Chandana1223 <chandana.k232@gmail.com>
2020-12-29 15:23:48 +0530

    Adding Employee wage per month

--- EmployeeWageProgram.sh
+++ EmployeeWageProgram.sh
@@ -4,19 +4,29 @@ echo "Welcome to Employee Wage Computation"
 
 isPartTime=1
 isFullTime=2
+totalSalary=0
 empRatePerHr=20
-randomCheck=$(( RANDOM % 3 ))
+numofWorkingDays=20
 
-case $empCheck in
-	$isFullTime)
-		empHrs=8
-                ;;
-	$isPartTime)
-		empHrs=4
-                ;;
-	*)
-		empHrs=0
-                ;;
-esac
+for (( day=1; day<=$numofWorkingDays; day++ ))
+do
 
-salary=$(( $empHrs * $empRatePerHr ))
+      empCheck=$(( RANDOM % 3 ))
+
+      case $empCheck in
+		$isFullTime)
+			empHrs=8
+                        ;;
+		$isPartTime)
+			empHrs=4
+                        ;;
+		*)
+			empHrs=0
+                        ;;
+	esac
+
+	salary=$(( $empHrs * $empRatePerHr ))
+	totalSalary=$(( $totalSalary + $salary ))
+
+done
+       echo "Employee wage per month:" $salary
